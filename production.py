53a4fe7b2d625856445175d60dbd6b670878d522
jacek <jacek@example.pl>
2018-05-05 17:43:33 +0200

    short documentation

--- production.py
+++ production.py
@@ -1,10 +1,51 @@
 from head import *
 
-
 hp_db = read_object('hp_db')
 
+""""
+hp_db to słownik, gdzie kluczem jest np 'HP:0000005', a wartością obiekt HP
 
-# HP:0012823
+Przydatną metodą jest funkcja tree(), która rysuje drzewno od rodzica na którym została wywołana w dół.
+Wyświetla ona id (klucz w słowniku), liczbę genów i nazwę
 
+Inne metody:
+    def get_children_ids(self)
+    Zwraca ids dzieci
+    
+    def get_children(self):
+    Zwraca dzieci danego węzła jako listę obiektów HP
+    
+    def get_parents_ids(self):
+    Zwraca ids rodziców
+    
+    def get_parents(self):
+    Zwraca rodziców danego węzła jako listę obiektów HP
+    
+    def get_genes(self, sort=False):
+    Zwraca listę genów związanych z danym węzłem
+    
+    def get_children_genes(self, unique=False, sort=False):
+    Zwraca listę genów związanych ze wszystkimi dzieciami danego węzła (bez genów węzła)
+    
+    def get_all_genes(self, unique=False, sort=False):
+    Zwwraca listę wszystkich genów (węzła i dzieci)
+    
+    def up(self, parent=''):
+    Pozwala przejść do dziecka.
+    
+    def down(self, child=''):
+    Pozwala przejść do rodzica
+    
+    def tree(self, depth=-1, drop_empty=False):
+    Rysuje drzewko.
+
+    def dump(self, all_genes=False):
+    Zapisuje geny do pliku
+Np:
+"""
+
+print(hp_db['HP:0000005'].tree(depth=1, drop_empty=True))
+
+print(hp_db['HP:0000005'].down(0).down('HP:0001452').get_genes())
 
 
